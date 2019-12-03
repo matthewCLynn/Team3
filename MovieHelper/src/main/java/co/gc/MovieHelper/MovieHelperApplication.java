987b5b3a9f5a513906707b70f0bed8a56507a343
matthewCLynn <matthewchristopherlynn@yahoo.com>
2019-12-03 13:28:38 -0500

    first commit

---
+++ MovieHelper/src/main/java/co/gc/MovieHelper/MovieHelperApplication.java
@@ -0,0 +1,13 @@
+package co.gc.MovieHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieHelperApplication.class, args);
+	}
+
+}
