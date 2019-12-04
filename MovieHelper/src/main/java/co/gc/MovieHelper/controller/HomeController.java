171f69b2565eec2e6b048baedb5aa401679d1963
matthewCLynn <matthewchristopherlynn@yahoo.com>
2019-12-04 13:27:44 -0500

    attempted to add movie list in controller

--- MovieHelper/src/main/java/co/gc/MovieHelper/controller/HomeController.java
+++ MovieHelper/src/main/java/co/gc/MovieHelper/controller/HomeController.java
@@ -1,29 +1,43 @@
 package co.gc.MovieHelper.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import co.gc.MovieHelper.beans.Results;
+
 @Controller
 public class HomeController
 {
-	@Value("${movie.token}")
-	String token;
+	@Value("${movie.key}")
+	String key;
 	
 	RestTemplate rt = new RestTemplate();
 	
 	@RequestMapping("/")
 	public ModelAndView homePage()
 	{
-		
-		
-		return new ModelAndView("index");
+		String url = "https://api.themoviedb.org/3/discover/movie?api_key=" + key;
+		Results output = rt.getForObject(url, Results.class);
+		return new ModelAndView("index", "test", output);
 	}
-//	
-//	@RequestMapping("search-movie")
-//	public ModelAndView (String searchCategory,)
+	
+	@RequestMapping("search-movie")
+	public ModelAndView searchDatabase(String filterChoice, String filterInput, String name)
+	{
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Token "+key);
+		headers.add(HttpHeaders.USER_AGENT, "Testing!");
+		headers.add("Accept",  MediaType.APPLICATION_JSON_VALUE);
+		
+		
+		
+		return null;
+	}
 	
 	
 
